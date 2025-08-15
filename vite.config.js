f6d3f48bec624ae640c0cda830c4ab9a0c5cb6a4
Arin Kumar <arinsha666@gmail.com>
2025-08-15 18:25:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+ "scripts": {
+  "build": "vite build",
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d dist"
+}
 // Replace with your repo name
 export default defineConfig({
   base: '/e-commerce-static/',
   plugins: [react()],
+  
+ 
 })
